b72a8a3175defabe0d80cdb198fa5d564a39a676
vadick0 <79231512+vadick0@users.noreply.github.com>
2021-03-01 15:12:18 +0200

    Init

---
+++ CarShop_vadimillia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarShop_vadimillia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
