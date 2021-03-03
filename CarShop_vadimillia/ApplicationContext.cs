d2d63ca8f10effba0ce2d81015e226e4313c2a80
vadick0 <79231512+vadick0@users.noreply.github.com>
2021-03-03 17:22:37 +0200

    +db

---
+++ CarShop_vadimillia/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using CarShop_vadimillia.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop_vadimillia
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
