6e3f26d8ea081fe93efa7d4ac9ee607234ded76d
vadick0 <79231512+vadick0@users.noreply.github.com>
2021-03-11 14:16:12 +0200

    Revert "Revert "vehcontroller""
    This reverts commit 79fbfed5fae83da16bdc30ca143ccd88420bcc4f.

---
+++ CarShop_vadimillia/Controllers/VehicleController.cs
@@ -0,0 +1,77 @@
+﻿using CarShop_vadimillia.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace CarShop_vadimillia.Controllers
+{
+    public class VehicleController : Controller
+    {
+        private readonly ApplicationContext _db;
+        IWebHostEnvironment _appEnvironment;
+        public VehicleController(ApplicationContext db, IWebHostEnvironment appEnvironment)
+        {
+            _db = db;
+            _appEnvironment = appEnvironment;
+        }
+        [HttpPost]
+        public IActionResult AddVehicle(string Mark, int BuildYear, string Description, int Price, IFormFile formFile)
+        {
+            try
+            {
+                if (formFile != null)
+                {
+                    string formfile = "/Files/" + formFile.FileName;
+                    using (var filestream = new FileStream(_appEnvironment.WebRootPath + formfile, FileMode.Create))
+                    {
+                        formFile.CopyTo(filestream);
+                    }
+                }
+                Vehicle veh = new Vehicle();
+                veh.Mark = Mark;
+                veh.BuildYear = BuildYear;
+                veh.Description = Description;
+                veh.Price = Price;
+                veh.Img = "/Files/" + formFile.FileName;
+                _db.Vehicles.Add(veh);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Error");
+            }
+        }
+
+        //public IActionResult Index(string description)
+        //{
+        //    try
+        //    {
+        //        if (description != null)
+        //        {
+        //            ViewBag.OrderByTitleAndDescription = _db.Vehicles.Where(x => x.Mark.Contains(description) || x.Description.Contains(description));
+        //            return View();
+        //        }
+        //        else
+        //        {
+        //            return View(false);
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        return RedirectToAction("Error");
+        //    }
+        //}
+        public IActionResult Index()
+        {
+            ViewBag.AllCars = _db.Vehicles.ToList();
+            return View();
+        }
+
+    }
+}
---
+++ CarShop_vadimillia/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarShop_vadimillia.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Mark { get; set; }
+        public int BuildYear { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Img { get; set; }
+    }
+}
